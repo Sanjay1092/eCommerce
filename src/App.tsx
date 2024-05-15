512c991a75e4a5048230b5815e86b7b911c5192e
Sanjay senthil <sanjay.senthil@getshopse.com>
2024-05-15 23:00:13 +0530

    add redux

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,10 @@
+import { Provider } from "react-redux";
+import store from "./store/store";
 function App() {
   return (
-    <>
+    <Provider store={store}>
       <h2 className="text-sm text-red-200">E-Commerce</h2>
-    </>
+    </Provider>
   );
 }
 
